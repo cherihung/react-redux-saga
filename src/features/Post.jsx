c80c3519212512d11dba31b74ef80cbf27fbba2d
Cheri Hung <cherihung@gmail.com>
2018-08-25 23:57:06 -0400

    separate Post out

---
+++ src/features/Post.jsx
@@ -0,0 +1,22 @@
+import styled from 'react-emotion';
+
+const Post = styled('div')({
+  padding: '1rem',
+  marginBottom: '0.5em',
+  h2: {
+    textTransform: 'capitalize',
+    margin: '0.5em 0 0',
+  },
+  p: {
+    padding: '0',
+    margin: '0',
+    fontSize: '1.5em',
+    lineHeight: '1.25em',
+  },
+  label: {
+    backgroundColor: 'beige',
+    padding: '0.1em',
+  },
+});
+
+export default Post;
